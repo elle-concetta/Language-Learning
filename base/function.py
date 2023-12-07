910a2046fb8407831362e5c508e591a676e5a718
Elizabeth Fassler <128157075+elle-concetta@users.noreply.github.com>
2023-12-07 05:16:10 -0500

    Update function.py

--- base/function.py
+++ base/function.py
@@ -2,16 +2,28 @@ import os
 import json
 import openai
 
+
 def lambda_handler(event, context):
     prompt = event["queryStringParameters"]["prompt"]
-    api_key = os.environ["API_KEY"]  # Access the API key from environment variables
-    openai.api_key = api_key
+    openai.api_key = os.getenv("API_KEY")  # Retrieve API key from environment variables
+
     model_engine = "gpt-3.5-turbo"
 
-    response = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=500)
-    text_result = response.choices[0].text
+    # Chat Completion endpoint for chat models
+    response = openai.ChatCompletion.create(
+        model=model_engine,
+        messages=[{"role": "system", "content": "You are a personal Gujarati language teacher."},
+                  {"role": "assistant", "content": "Hi there, I’m Jolly. Welcome to the first step of your "
+                                                   "Gujarati language journey! Are you ready? Or should I say "
+                                                   "'Tu taiyaar chhe?' Type 'Yes' to start your first lesson."},
+                  {"role": "user", "content": prompt}]
+    )
+
+    # Extracting the text from the response
+    text_result = response.choices[0].message['content']
 
     return {
         'statusCode': 200,
         'body': json.dumps(text_result, ensure_ascii=False, indent=4)
     }
+
