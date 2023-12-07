3264acfdea2660b9f0f5ba628a553e6b6dc230b0
Elizabeth Fassler <128157075+elle-concetta@users.noreply.github.com>
2023-12-07 08:13:43 -0500

    Update app.py

--- base/app.py
+++ base/app.py
@@ -1,25 +1,37 @@
 import streamlit as st
-import requests
 from PIL import Image
-import datetime as time
+import requests
 
-st.title("Bhasha Bun Language Learning Chatbot")
+st.title("Gujarati Language Tutor")
 
-img = Image.open(r'../images/background01.png')
-st.image(img)
-key_prompt = st.text_input("Prompt: ")
+# Load and display images
+img = Image.open(r'./images/jolly01.png')
+st.sidebar.image(img, caption='Jolly')
 
-if st.button("Ask to ChatGPT.."):
-    # The execution time limit for an AWS Lambda function is approximately 60 seconds,
-    # and if the function takes longer than that to process a request,
-    # it will not be able to provide a response.
-    if len(key_prompt) > 250:
-        st.error("Try more shorter prompt")
+gptimg = Image.open(r'./images/background01.png')
+st.image(gptimg)
+
+# Initialize the chat messages history with a welcome message
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "system", "content": "Welcome to the Gujarati Language Tutor. How can I assist you today?"}]
+
+# Prompt for user input
+user_prompt = st.text_input("Enter your message:", key="chat_input")
+if st.button("Send"):
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+    # Call your API Gateway URL
+    url = "/base"
+    response = requests.get(url, params={"prompt": user_prompt})
+
+    if response.status_code == 200:
+        lambda_response = response.json() # Parse response from Lambda
+        st.session_state.messages.append({"role": "assistant", "content": lambda_response['message']})
     else:
-        with st.spinner("Your answer is currently being generated..."):
-            time.sleep(5)
-        url = "/base"
+        st.error("Failed to get response from Lambda function")
 
-        responsegpt = requests.get(url, params={"prompt": key_prompt})
-
-        st.success(responsegpt.json())
+# Display chat messages
+for message in st.session_state.messages:
+    with st.container():
+        st.write(f"{message['role'].title()}: {message['content']}")
